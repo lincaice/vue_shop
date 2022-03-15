ff5c11d0e90e5453d66bfdb03c78cb62b69c537a
lincaice <2328462307@qq.com>
2022-03-15 13:20:09 +0800

    登录功能，实现

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,24 +3,38 @@
         <div class="login_box">
             <!-- 头像区域 -->
             <div class="avatar_box">
-                <img src="../assets/logo.png">
+                <img src="../assets/logo.png" />
             </div>
             <!-- 登录表单区 -->
-            <el-form label-width="0px" class="login_form" :model="loginForm">
+            <el-form
+                class="login_form"
+                label-width="0px"
+                :model="loginForm"
+                ref="loginFormRef"
+                :rules="loginFormRules"
+            >
                 <!-- 用户名 -->
-                <el-form-item>
-                    <el-input v-model="loginForm.username" prefix-icon="el-icon-user"></el-input>
+                <el-form-item prop="username">
+                    <el-input
+                        v-model="loginForm.username"
+                        prefix-icon="el-icon-user"
+                    ></el-input>
                 </el-form-item>
                 <!-- 密码 -->
-                <el-form-item>
-                    <el-input v-model="loginForm.password" type="password" prefix-icon="el-icon-lock"></el-input>
+                <el-form-item prop="password">
+                    <el-input
+                        v-model="loginForm.password"
+                        type="password"
+                        prefix-icon="el-icon-lock"
+                    ></el-input>
                 </el-form-item>
                 <!-- 按钮区域 -->
                 <el-form-item class="btns">
-                    <el-button type="primary">登录</el-button>
-                    <el-button type="info">重置</el-button>
+                    <el-button type="primary" @click="login">登录</el-button>
+                    <el-button type="info" @click="resetLoginFrom"
+                        >重置</el-button
+                    >
                 </el-form-item>
-
             </el-form>
         </div>
     </div>
@@ -30,11 +44,65 @@
 export default {
     data() {
         return {
+            //表单信息
             loginForm: {
-                username: '',
-                password: ''
-            }
-        }
+                username: "admin",
+                password: "123456",
+            },
+            // 表单验证规则
+            loginFormRules: {
+                // 验证用户名
+                username: [
+                    {
+                        required: true,
+                        message: "请输入登录名称",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 3,
+                        max: 10,
+                        message: "长度在 3 到 10 个字符",
+                        trigger: "blur",
+                    },
+                ],
+                // 验证密码
+                password: [
+                    {
+                        required: true,
+                        message: "请输入登录密码",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 6,
+                        max: 15,
+                        message: "长度在 6 到 15 个字符",
+                        trigger: "blur",
+                    },
+                ],
+            },
+        };
+    },
+    methods: {
+        resetLoginFrom() {
+            this.$refs.loginFormRef.resetFields();
+            console.log();
+        },
+        login() {
+            this.$refs.loginFormRef.validate(async (valid) => {
+                if (valid) {
+                    const { data: res } = await this.$http.post(
+                        "login",
+                        this.loginForm
+                    );
+                    if (res.meta.status != 200) {
+                        return this.$message.error("登录失败");
+                    }
+                    this.$message.success("登录成功");
+                    window.sessionStorage.setItem('token',res.data.token);
+                    this.$router.push('/home');
+                }
+            });
+        },
     },
 };
 </script>
@@ -72,16 +140,15 @@ export default {
     }
 }
 
-.login_form{
+.login_form {
     position: absolute;
     bottom: 0;
     width: 100%;
     padding: 15px;
     box-sizing: border-box;
 }
-.btns{
+.btns {
     display: flex;
     justify-content: space-around;
 }
-
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
 
 const router = new VueRouter({
   routes: [
@@ -16,8 +16,23 @@ const router = new VueRouter({
     {
       path: '/login',
       component: Login
+    },
+    {
+      path: '/home',
+      component: Home
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    return next()
+  }
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) {
+    return next('/login')
+  }
+  next()
 })
 
 export default router
